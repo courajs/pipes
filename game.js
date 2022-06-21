24d0bc90de4e36c7814069071496eb8562f40bac
Aaron Sikes <aaron@sikes.io>
2022-06-21 13:54:59 -0400

    Change Array prototype extensions to be non-enumerable

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
-Array.prototype.random_index = function(){
-		var index = Math.floor( Math.random() * this.length);
-		if (index === this.length)
-			index -= 1;
-		return index;
-	}
-
-
-Array.prototype.random = function(){
-	var index = this.random_index();
-	return this[index];
-}
+Object.defineProperty(Array.prototype, 'random_index', {
+  value() {
+    return Math.floor(Math.random() * this.length);
+  }
+});
+Object.defineProperty(Array.prototype, 'random', {
+  value() {
+    return this[this.random_index()];
+  }
+});
 
 function Cell(x, y, board){
 	this.x = x;
